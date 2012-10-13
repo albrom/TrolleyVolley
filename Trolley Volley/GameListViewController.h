8f5e32227e9f8d5e6ab8ffb5f920ac868a81cea8
albrom <albrom@yandex.ru>
2012-10-13 15:13:33 +0700

    Two controllers

---
+++ Trolley Volley/GameListViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameListViewController.h
+//  Trolley Volley
+//
+//  Created by MacBookPro on 13.10.12.
+//  Copyright (c) 2012 VitBro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameListViewController : UIViewController
+
+@end
---
+++ Trolley Volley/GameListViewController.m
@@ -0,0 +1,44 @@
+//
+//  GameListViewController.m
+//  Trolley Volley
+//
+//  Created by MacBookPro on 13.10.12.
+//  Copyright (c) 2012 VitBro. All rights reserved.
+//
+
+#import "GameListViewController.h"
+
+@interface GameListViewController ()
+
+@end
+
+@implementation GameListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
