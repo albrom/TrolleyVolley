638971d8a001fbc5e77c45b25d41d14205c2829d
MacBookAir <interrium2@MacBook-Air-MacBookAir.local>
2012-10-13 12:29:50 +0700

    Initial Commit

---
+++ Trolley Volley/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Trolley Volley
+//
+//  Created by MacBookAir on 13.10.12.
+//  Copyright (c) 2012 VitBro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Trolley Volley/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Trolley Volley
+//
+//  Created by MacBookAir on 13.10.12.
+//  Copyright (c) 2012 VitBro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
